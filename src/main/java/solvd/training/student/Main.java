1d8c6b04b4994c33370db035d84f2e37cb07d962
Ania123678 <149054948+AnnaKLabAuto@users.noreply.github.com>
2023-11-21 09:38:46 +0100

    handle IOException exception

--- src/main/java/solvd/training/student/Main.java
+++ src/main/java/solvd/training/student/Main.java
@@ -12,9 +12,14 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String filePath = Main.class.getClassLoader().getResource("file.txt").getPath();
-        String text = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
-        Set<String> uniqueWords = new HashSet<>(List.of(StringUtils.split(text, " ")));
-        FileUtils.writeStringToFile(new File("output.txt"), String.valueOf(uniqueWords.size()), StandardCharsets.UTF_8);
+        try {
+            String filePath = Main.class.getClassLoader().getResource("file.txt").getPath();
+            String text = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
+            Set<String> uniqueWords = new HashSet<>(List.of(StringUtils.split(text, " ")));
+            FileUtils.writeStringToFile(new File("output.txt"), String.valueOf(uniqueWords.size()), StandardCharsets.UTF_8);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
