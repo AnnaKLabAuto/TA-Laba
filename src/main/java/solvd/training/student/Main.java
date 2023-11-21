678df219b6032f43b1ad6775d050517a5828acef
Ania123678 <149054948+AnnaKLabAuto@users.noreply.github.com>
2023-11-21 14:21:56 +0100

    refactor exception handling in Main.java

--- src/main/java/solvd/training/student/Main.java
+++ src/main/java/solvd/training/student/Main.java
@@ -9,17 +9,20 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         try {
             String filePath = Main.class.getClassLoader().getResource("file.txt").getPath();
-            String text = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
-            Set<String> uniqueWords = new HashSet<>(List.of(StringUtils.split(text, " ")));
-            FileUtils.writeStringToFile(new File("output.txt"), String.valueOf(uniqueWords.size()), StandardCharsets.UTF_8);
-
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                String text = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
+                Set<String> uniqueWords = new HashSet<>(List.of(StringUtils.split(text, " ")));
+                FileUtils.writeStringToFile(new File("output.txt"), String.valueOf(uniqueWords.size()), StandardCharsets.UTF_8);
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Error reading or writing files", e);
         }
     }
 }
